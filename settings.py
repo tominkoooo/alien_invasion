e01029cded4b381af16630c795e42d4756810a86
Tomasz Ciborowski <jowisz108@gmail.com>
2023-01-26 21:27:59 +0100

    Added Settings class
    Settings class is needed to store all settings used in game creation.

---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings:
+    """
+    Class created for storing game settings.
+    """
+
+    def __init__(self):
+        """
+        Initialization of game settings.
+        """
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (255, 255, 255)
